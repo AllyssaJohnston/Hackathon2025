29170cffc6c9f5cff782b1cc57f243fc83f2c6d0
AllyssaJohnston <168232004+AllyssaJohnston@users.noreply.github.com>
2025-04-25 21:24:54 -0700

    updating stash

---
+++ Project Hackathon 2D/Assets/Scripts/NPCMovement.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NPCMovement : MonoBehaviour
+{
+    public GameObject target;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        var agent = GetComponent<NavMeshAgent>();
+        agent.updateRotation = false;
+        agent.updateUpAxis = false;
+        agent.destination = target.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
