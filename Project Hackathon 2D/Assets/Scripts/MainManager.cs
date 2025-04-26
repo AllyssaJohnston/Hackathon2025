37ee6c46c7d05367bbe9a490b3cf6fd3b815de31
suspiciouslyBee <suspiciously@bee.movie>
2025-04-25 21:07:26 -0700

    main manager is

--- Project Hackathon 2D/Assets/Scripts/MainManager.cs
+++ Project Hackathon 2D/Assets/Scripts/MainManager.cs
@@ -2,6 +2,22 @@ using UnityEngine;
 
 public class MainManager : MonoBehaviour
 {
+    public static MainManager Instance;
+
+
+    //make this a singleton
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
